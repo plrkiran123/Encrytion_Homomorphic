e30ac59b94bce93982652f4a5d95317c12f3bac2
plrkiran123 <plrkiran54@gmail.com>
2024-07-24 22:10:39 +0200

    Update HE_chaincode.go

--- HE_chaincode.go
+++ HE_chaincode.go
@@ -1,81 +1,89 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 type SmartContract struct {
-	contractapi.Contract
+    contractapi.Contract
 }
 
 type EncryptedData struct {
-	ID            string `json:"id"`
-	EncryptedText string `json:"encryptedText"`
+    ID            string `json:"id"`
+    EncryptedText string `json:"encryptedText"`
 }
 
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	data := []EncryptedData{
-		{ID: "data1", EncryptedText: "example_encrypted_data1"},
-		{ID: "data2", EncryptedText: "example_encrypted_data2"},
-	}
+    data := []EncryptedData{
+        {ID: "data1", EncryptedText: "example_encrypted_data1"},
+        {ID: "data2", EncryptedText: "example_encrypted_data2"},
+    }
 
-	for _, datum := range data {
-		dataJSON, err := json.Marshal(datum)
-		if err != nil {
-			return err
-		}
+    for _, datum := range data {
+        dataJSON, err := json.Marshal(datum)
+        if err != nil {
+            return err
+        }
 
-		err = ctx.GetStub().PutState(datum.ID, dataJSON)
-		if err != nil {
-			return fmt.Errorf("failed to put data. %v", err)
-		}
-	}
+        err = ctx.GetStub().PutState(datum.ID, dataJSON)
+        if err != nil {
+            return fmt.Errorf("failed to put data to world state. %v", err)
+        }
+    }
 
-	return nil
+    return nil
 }
 
-func (s *SmartContract) StoreEncryptedData(ctx contractapi.TransactionContextInterface, id string, encryptedText string) error {
-	data := EncryptedData{
-		ID:            id,
-		EncryptedText: encryptedText,
-	}
-	dataJSON, err := json.Marshal(data)
-	if err != nil {
-		return err
-	}
+func (s *SmartContract) StoreEncryptedData(ctx contractapi.TransactionContextInterface, id string) error {
+    // Replace with actual path
+    encryptedText, err := ioutil.ReadFile("/path/to/encrypted_data.bin")
+    if err != nil {
+        log.Fatalf("failed to read encrypted data: %v", err)
+    }
 
-	return ctx.GetStub().PutState(id, dataJSON)
+    data := EncryptedData{
+        ID:            id,
+        EncryptedText: string(encryptedText),
+    }
+
+    dataJSON, err := json.Marshal(data)
+    if err != nil {
+        return err
+    }
+
+    return ctx.GetStub().PutState(id, dataJSON)
 }
 
 func (s *SmartContract) GetEncryptedData(ctx contractapi.TransactionContextInterface, id string) (*EncryptedData, error) {
-	dataJSON, err := ctx.GetStub().GetState(id)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read : %v", err)
-	}
-	if dataJSON == nil {
-		return nil, fmt.Errorf("the data %s does not exist", id)
-	}
+    dataJSON, err := ctx.GetStub().GetState(id)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read from world state: %v", err)
+    }
+    if dataJSON == nil {
+        return nil, fmt.Errorf("the data %s does not exist", id)
+    }
 
-	var data EncryptedData
-	err = json.Unmarshal(dataJSON, &data)
-	if err != nil {
-		return nil, err
-	}
+    var data EncryptedData
+    err = json.Unmarshal(dataJSON, &data)
+    if err != nil {
+        return nil, err
+    }
 
-	return &data, nil
+    return &data, nil
 }
 
 func main() {
-	chaincode, err := contractapi.NewChaincode(new(SmartContract))
-	if err != nil {
-		fmt.Printf("Error creating chaincode: %v", err)
-		return
-	}
+    chaincode, err := contractapi.NewChaincode(new(SmartContract))
+    if err != nil {
+        fmt.Printf("Error creating chaincode: %v", err)
+        return
+    }
 
-	if err := chaincode.Start(); err != nil {
-		fmt.Printf("Error starting chaincode: %v", err)
-	}
+    if err := chaincode.Start(); err != nil {
+        fmt.Printf("Error starting chaincode: %v", err)
+    }
 }
